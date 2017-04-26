2906c8caff5c63bc7b9cfe7483e17b9f7a151f08
Christopher Kolodziejczyk <cmk1523@gmail.com>
2017-04-26 01:03:17 -0400

    na

--- install.sh
+++ install.sh
@@ -23,14 +23,42 @@ sudo /bin/systemctl daemon-reload
 #-Xmx512m
 
 #sudo systemctl start elasticsearch.service
+#sudo systemctl restart elasticsearch.service
 #sudo systemctl stop elasticsearch.service
 
 
 
-wget -N https://artifacts.elastic.co/downloads/kibana/kibana-5.3.1-linux-x86_64.tar.gz
-rm -rf kibana-5.3.1-linux-x86_64
-tar -xf kibana-5.3.1-linux-x86_64.tar.gz
+
+#wget -N https://artifacts.elastic.co/downloads/kibana/kibana-5.3.1-linux-x86_64.tar.gz
+wget -N https://artifacts.elastic.co/downloads/logstash/logstash-5.3.1.deb
+sudo dpkg -i logstash-5.3.1.deb
 #sudo ./kibana-5.3.1-linux-x86_64/bin/kibana-plugin install x-pack
 
 #nano ./kibana-5.3.1-linux-x86_64/config/kibana.yml
 # reminder: set host to 0.0.0.0
+
+
+
+
+
+#logstash
+#logstash config files: /etc/logstash/conf.d/
+#logstash log files: /var/log/logstash/logstash-plain.log
+
+#sudo systemctl start logstash.service
+#sudo systemctl restart logstash.service
+#sudo systemctl stop logstash.service
+
+
+# make arm-linux libjffi file
+#http://www.intranetofstuff.com/2016/06/24/logstash.html
+
+#sudo mkdir -p /opt/local/src
+#cd /opt/local/src
+#sudo git clone https://github.com/jnr/jffi.git
+#sudo apt-get install ant zip
+
+#cd jffi
+#sudo ant jar
+#sudo mv /usr/share/logstash/vendor/jruby/lib/jni/arm-Linux/libjffi-1.2.so /usr/share/logstash/vendor/jruby/lib/jni/arm-Linux/libjffi-1.2.so~
+#sudo cp build/jni/libjffi-1.2.so /usr/share/logstash/vendor/jruby/lib/jni/arm-Linux/libjffi-1.2.so
